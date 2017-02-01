9a6a2dbfedfa2c99346d9d29fcd380d00b89fca1
haoyujin <haoyujin2012@gmail.com>
2017-02-01 22:55:29 +0800

    implement part4 adminpanel different from job

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -15,7 +15,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def create
-    @job = job.new(job_params)
+    @job = Job.new(job_params)
     if @job.save
       redirect_to admin_jobs_path
     else
@@ -44,6 +44,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -16,7 +16,7 @@ class JobsController < ApplicationController
     end
   end
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
   def edit
     @job = Job.find(params[:id])
@@ -38,6 +38,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
